e9b763e2b028b7a3e046c24d1411244340bd12c3
Vishal <vishal.v@tripfactory.travel>
2026-04-21 15:12:42 +0530

    solved `color_to_rgb` from `fundamentals/easy`

--- fundamentals/src/easy/color_enum.rs
+++ fundamentals/src/easy/color_enum.rs
@@ -16,5 +16,10 @@ pub enum Color {
 }
 
 pub fn color_to_rgb(color: Color) -> (u8, u8, u8) {
-    todo!()
+    match color {
+        Color::Red => (255, 0, 0),
+        Color::Green => (0, 255, 0),
+        Color::Blue => (0, 0, 255),
+        Color::Custom(r, g, b) => (r, g, b),
+    }
 }
